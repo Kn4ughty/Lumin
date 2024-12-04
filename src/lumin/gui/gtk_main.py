8e7b633763b6d25a5b90f7d617c87443619f8885
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2024-12-04 21:45:01 +1100

    switch to gtk4 or something

--- src/lumin/gui/gtk_main.py
+++ src/lumin/gui/gtk_main.py
@@ -3,16 +3,18 @@ from typing import Callable
 
 import gi
 
-gi.require_version("Gtk","3.0")
+gi.require_version("Gtk","4.0")
 from gi.repository import Gtk, Gdk  # noqa: E402
 
 
 class GTKApp:
     def __init__(self, on_search_activate: Callable, on_search_text_changed: Callable):
         # Create a window
-        self.window = Gtk.Window(title="Lumin Search")
+        self.window = Gtk.Window(title="")
         self.window.set_default_size(600, 200)
         self.window.set_resizable(False)
+        self.window.set_decorated(False)
+        self.window.set_titlebar()
 
         # Load css
         css_provider = Gtk.CssProvider()
@@ -20,14 +22,14 @@ class GTKApp:
         css_provider.load_from_path(str(theme_path))
 
         # Apply the CSS to the window
-        Gtk.StyleContext.add_provider_for_screen(
-            Gdk.Screen.get_default(),
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(),
             css_provider,
             Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
         )
 
         self.box = Gtk.Box(spacing=6)
-        self.window.add(self.box)
+        self.window.set_child(self.box)
 
         # Add a simple label as a placeholder
         self.label = Gtk.Entry()
@@ -35,18 +37,19 @@ class GTKApp:
         
         self.label.connect("changed", on_search_text_changed)
 
-        self.box.pack_start(self.label, True, True, 0)
+        self.box.append(self.label)
 
         self.button = Gtk.Button(label="button!")
         self.button.connect("clicked", self.on_button_clicked)
-        self.box.pack_start(self.button, True, True, 0)
+        self.box.append(self.button)
         
         # Connect the close event
-        self.window.connect("destroy", Gtk.main_quit)
+        # self.window.connect("destroy", Gtk.main_quit)
 
     def run(self):
-        self.window.show_all()  # Show the window
-        Gtk.main()
+        self.window.present()  # Show the window
+        # Gtk.main()
+        pass
 
     def on_button_clicked(self, widet):
         print("Buttoned!")
