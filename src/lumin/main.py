2cd75df0f5763284feae842256df2fc9216978f6
Naught <77908661+Kn4ughty@users.noreply.github.com>
2024-12-19 10:41:24 +1100

    Add logging

--- src/lumin/main.py
+++ src/lumin/main.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+import logging as log
 
 # Add the src/ directory to sys.path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
@@ -13,28 +14,39 @@ import gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk  # noqa: E402
 
+# log.basicConfig(level=log.DEBUG)
+log.basicConfig(
+    level=log.INFO,
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%H:%M:%S",
+)
+log.info("ASKLDJLKSDFJLKSDFJj")
+
 
 def main():
     global app
+    log.info("Gui being initialised")
     app = MyApp(on_search_text_changed, on_search_activate)
+    log.info("Running GUI")
     app.run()
 
 
 def on_search_activate(search_box):
-    print("Search Activated")
-    pass
+    log.info("Search box activated")
 
 
 def on_open():
-    print("woah i was opened")
+    log.info("Thing opened")
 
 
 def on_search_text_changed(search_box):
-    print("Search text changed")
+    log.info(f"Seach entry text changed. {search_box}.text = {search_box.get_text()}")
 
     text = search_box.get_text()
 
     if text == "":
+        log.info("Search text was empty. Showing empty results")
         app.update_results(Gtk.Box())  # Make results empty
         return
 
@@ -45,8 +57,6 @@ def on_search_text_changed(search_box):
 
     result_box = result.result_list_to_gtkbox(result_list)
     app.update_results(result_box)
-
-    print(search_box.get_text())
 
 
 if __name__ == "__main__":
--- src/lumin/types/result.py
+++ src/lumin/types/result.py
@@ -1,10 +1,13 @@
 from typing import Callable, List
 from dataclasses import dataclass
+import logging as log
 
 import gi
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk, Gdk  # noqa: E402
+
+log.getLogger(__name__)
 
 
 @dataclass
@@ -15,9 +18,12 @@ class Result:
 
 
 def result_list_to_gtkbox(result_list: List[Result]):
+    log.info("Turning results list into a gtkbox.")
+    log.debug(f"result_list = {result_list}")
     main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=0)
 
     for item in result_list:
+        log.debug(f"For loop. item = {item}")
         frame = Gtk.Frame()
         frame.set_label_align(0.0)
 
