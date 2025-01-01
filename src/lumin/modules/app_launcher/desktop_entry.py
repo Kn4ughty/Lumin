a180d066dfc4fe28d852073bf0a935331acf8bc7
Naught <77908661+Kn4ughty@users.noreply.github.com>
2025-01-01 20:17:58 +1100

    refactor desktop entry processing, and finish(?) .desktop reading functionality

--- src/lumin/modules/app_launcher/desktop_entry.py
+++ src/lumin/modules/app_launcher/desktop_entry.py
@@ -1,109 +1,102 @@
-from dataclasses import dataclass, field
 from pathlib import Path
 from typing import List
 import time
+import os
 
 import logging as log
 
-# TODO. Work this out
+
 # https://specifications.freedesktop.org/icon-theme-spec/latest/
+def parse_xdg_file(lines: List[str]) -> dict:
+    """example file
+    [Icon Theme]
+    Name=Adwaita
+    Comment=The Only One
+    Example=folder
+    Inherits=AdwaitaLegacy,hicolor
+    Hidden=true
 
+    # Directory list
+    Directories=16x16/actions,16x16/apps
 
-class AppIcon():
-    pass
+    [16x16/actions]
+    Context=Actions
+    Size=16
+    Type=Fixed
 
-
-def get_all_desktop_apps(search_dir: Path) -> List[dict]:
-
-    start_time = time.time()
-
-    # Get all .destop files in search_dir
-    files = [f for f in search_dir.glob("*.desktop")]
-
-    entries = []
-
-    for file in files:
-        entries.append(parse_desktop_file(file))
-
-    log.info(f"Time to parse all .desktop files: {
-             (time.time() - start_time)*1000:.3f}ms")
-    return entries
-
-
-def parse_desktop_file(file: Path) -> dict:
-    ''' example file
-    [Desktop Entry]
-    Version=1.0
-    Name=VLC media player
-    GenericName=Media player
-    Comment=Read, capture, broadcast your multimedia streams
-    Name[af]=VLC-mediaspeler
-    GenericName[af]=Mediaspeler
-    ... 
-     More names in different languages
-    ...
-    Exec=/usr/bin/vlc --started-from-file %U
-    TryExec=/usr/bin/vlc
-    Icon=vlc
-    Terminal=false
-    Type=Application
-    '''
+    [16x16/apps]
+    Context=Applications
+    Size=16
+    Type=Fixed
+    """
 
     entry = {}
 
-    with open(file, 'r') as f:
-        lines = f.readlines()
-
-    log.debug(f"Parsing desktop file: {file} with contents {lines}")
-
-    in_desktop_entry_group_header = False
     for line in lines:
-        if line[0] == '#':
+        if line[0] == "#":
             log.debug(f"Skipping comment line: {line}")
             continue
 
-        if line == '\n':
+        if line == "\n":
             log.debug(f"Skipping empty line: {line}")
             continue
 
-        if line == '[Desktop Entry]\n':
-            in_desktop_entry_group_header = True
-            log.debug(f"Found desktop entry group header {line}")
-            continue
-
-        if line[0] == '[':
+        if line[0] == "[":  # ] my editor doesnt understand the bracket is quoted
             log.debug(f"ignoring unknown group header {line}")
             continue
 
-        if not in_desktop_entry_group_header:
-            log.warning(f"Desktop entry group header missing. Line: \'{
-                        line}\' \n full file: {lines}")
-            log.warning(f"Invalid .deskop file? Path {file}")
+        data = line.split("=", 1)
 
-        # At this point there should only be key value pairs in the format
-        # Key=Value
-
-        # Set max split to 1, to only match first =
-        # This is because exec values could include = as part of args
-        # i.e Exec=/usr/bin/example --arg1=1 --arg2=2
-        data = line.split('=', 1)
-
-        # Remove trailing whitespace
+        # Remove any trailing whitespace
         key = data[0].strip()
         value = data[1].strip()
 
         entry[key] = value
 
-        match key:
-            case "Hidden":
-                if value == "true":
-                    return {}
-                else:
-                    entry[key] = value
-            case "Path":
-                entry[key] = Path(value)
-
     return entry
 
 
-result = get_all_desktop_apps(Path("/usr/share/applications"))
+def get_XDG_DATA_DIRS() -> List[Path]:
+    default_dir = "/usr/share/"
+    XDG_DATA_DIRS = os.getenv("XDG_DATA_DIRS", default_dir)
+
+    dirs = []
+
+    if XDG_DATA_DIRS is None:
+        log.warning(f"XDG_DATA_DIRS not set. Defaulting to {default_dir}")
+
+    dirs = [Path(dir) for dir in XDG_DATA_DIRS.split(":")]
+    log.debug(f"found XDG_DATA_DIRS: {dirs}")
+
+    return dirs
+
+
+def get_all_desktop_apps() -> List[dict]:
+    start_time = time.time()
+
+    dirs = [dir.joinpath("applications") for dir in get_XDG_DATA_DIRS()]
+
+    entries = []
+
+    for search_dir in dirs:
+        files = [f for f in search_dir.glob("*.desktop")]
+
+        for file in files:
+            log.debug(f"Reading .desktop file: {file}")
+
+            with open(file, "r") as f:
+                lines = f.readlines()
+
+            entries.append(parse_xdg_file(lines))
+
+    log.info(
+        f"Time to parse all .desktop files: {
+             (time.time() - start_time)*1000:.3f}ms"
+    )
+    # On m1 mac it takes about 9ms. \pm 1ms
+
+    return entries
+
+
+result = get_all_desktop_apps()
+# print(result)
