08ce23805f66dc753fddb9a28ca2d0e7982d92a3
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-07-29 10:36:18 +1000

    fix small bug in text colouring

--- src/lumin/modules/app_launcher/main.py
+++ src/lumin/modules/app_launcher/main.py
@@ -56,9 +56,9 @@ def search() -> Gtk.Box:
                 apps = macos_search.get_app_file_paths()
             result_list = macos_search.get_macos_apps(apps)
         elif g.PLATFORM_OS == "linux":
-            # Previously I had done all of the .desktop parsing myself, which was actually quite fun. 
+            # Previously I had done all of the .desktop parsing myself, which was actually quite fun.
             # Then I found that GTK had a function for that built in. >:|
-            # However it seems to be missing some features (like desktop actions) 
+            # However it seems to be missing some features (like desktop actions)
             apps = Gio.AppInfo.get_all()
             result_list = get_linux_apps()
         # No else needed as result list is empty
