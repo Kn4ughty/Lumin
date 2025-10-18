1cb231fd4ccd81c80d17c3470319515ec47562bc
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-10-18 14:44:25 +1100

    add flamegraph profiling

--- benches/get_desktop_app.rs
+++ benches/get_desktop_app.rs
@@ -1,10 +1,17 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 
+use pprof::criterion::{Output, PProfProfiler};
+use pprof::flamegraph;
+
 use lumin::apps::get_apps;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("get apps", |b| b.iter(|| get_apps()));
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(100, Output::Flamegraph(Some(flamegraph::Options::default()))));
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+case $1 in
+    fg)
+        cargo bench --bench get_desktop_app -- --profile-time=5
+        xdg-open target/criterion/get\ apps/profile/flamegraph.svg
+esac
