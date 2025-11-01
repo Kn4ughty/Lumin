7c971e1c1274a303da57dafa958ae629aee03b89
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-11-02 09:37:47 +1100

    Fix dead_code warnings for drun

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![deny(clippy::unwrap_used)]
 pub mod apps;
 pub mod calculator;
+pub mod drun;
 pub mod module;
 pub mod serworse;
 pub mod websearch;
 pub mod widglets;
 
 mod constants;
-mod drun;
 mod message;
 mod util;
