fcfac445e55385f7f57096d6a6dc11fb91980ccf
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2025-11-02 15:49:21 +1100

    fix flamegraph command in run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 case $1 in
     fg)
-        cargo bench --bench get_desktop_app -- --profile-time=5
+        cargo bench --bench get_desktop_apps -- --profile-time=5
         xdg-open target/criterion/get\ apps/profile/flamegraph.svg
         ;;
     tt)
